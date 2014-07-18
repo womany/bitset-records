fd18de2cb20b56fdd0266a7a0d5469c08f643217
wildjcrt <wildjcrt@yahoo.com.tw>
2014-07-18 15:56:38 +0800

    bitset-records, #initialize and #new create

--- lib/bitset-records.rb
+++ lib/bitset-records.rb
@@ -1,5 +1,14 @@
 require "version"
 
 class BitsetRecords
-  # Your code goes here...
+  attr_accessor :bit2str
+
+  def initialize(length)
+    bit_array = Array.new(length+1, 0)
+    bit_array[0] = 1
+    @bit2str = bit_array.join
+  end
+  def value
+    @bit2str
+  end
 end
