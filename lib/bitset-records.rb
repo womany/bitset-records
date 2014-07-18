75904d19703ed6cc6a61a35bf89dcf926e23b5ff
wildjcrt <wildjcrt@yahoo.com.tw>
2014-07-18 15:58:20 +0800

    bitset-records, #add, #remove and #quantity create

--- lib/bitset-records.rb
+++ lib/bitset-records.rb
@@ -8,7 +8,30 @@ class BitsetRecords
     bit_array[0] = 1
     @bit2str = bit_array.join
   end
+
+  def add(*args)
+    args.compact!
+    args.each do |id|
+      @bit2str[id] = '1'
+    end
+
+    @bit2str
+  end
+
+  def remove(*args)
+    args.compact!
+    args.each do |id|
+      @bit2str[id] = '0'
+    end
+
+    @bit2str
+  end
+
   def value
     @bit2str
+  end
+
+  def quantity
+    @bit2str.count('1') - 1
   end
 end
